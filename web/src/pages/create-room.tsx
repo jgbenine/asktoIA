bf24a7e35df57747c9851664bb720942a4e28d68
jgbenine <joaobenine@gmail.com>
2025-07-13 22:25:51 -0300

    feat: init connection backend

--- web/src/pages/create-room.tsx
+++ web/src/pages/create-room.tsx
@@ -1,12 +1,34 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 
+type GetRoomsAPIResponse = Array<{
+  id: string;
+  name: string;
+}>;
+
 export function CreateRoom() {
+  const { data, isLoading } = useQuery({
+    queryKey: ["get-rooms"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/rooms");
+      const result: GetRoomsAPIResponse = await response.json();
+
+      return result;
+    },
+  });
+
   return (
     <div>
-      Room
-      <Link to="/room" className="underline">
-        Acessar sala
-      </Link>
+      Create room
+      {isLoading && <p>Carregando...</p>}
+      <div className="flex flex-col gap-0.5">
+        {data &&
+          data?.map((room) => (
+            <Link to={`/room/${room.id}`} key={room.id}>
+              {room.name}
+            </Link>
+          ))}
+      </div>
     </div>
   );
 }
--- web/src/pages/room.tsx
+++ web/src/pages/room.tsx
@@ -1,5 +1,15 @@
-import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+
+type RoomParams = {
+  roomId: string;
+};
 
 export function Room() {
+  const params = useParams<RoomParams>();
+
+  if (!params.roomId) {
+    return <Navigate to="/" replace />;
+  }
+
   return <div>Room</div>;
 }
