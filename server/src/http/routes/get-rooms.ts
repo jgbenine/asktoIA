7fe655216f92b408f2859ffb2a79f0f19cfdb84d
jgbenine <joaobenine@gmail.com>
2025-07-14 14:28:31 -0300

    feat: adding count questions in select rooms

--- server/src/http/routes/get-rooms.ts
+++ server/src/http/routes/get-rooms.ts
@@ -1,12 +1,21 @@
 import type { FastifyPluginCallbackZod } from 'fastify-type-provider-zod'
 import { db } from '../../db/connection.ts'
 import { schema } from '../../db/schema/index.ts'
+import { count, eq, name } from 'drizzle-orm'
+import { questions } from '../../db/schema/questions.ts'
 
 export const getRoomsRoute: FastifyPluginCallbackZod = (app) => {
   app.get('/rooms', async () => {
     const results = await db
-      .select()
+      .select({
+        id: schema.rooms.id,
+        name: schema.rooms.id,
+        createdAt: schema.rooms.createdAt,
+        quetionsCount: count(questions.id)
+      })
       .from(schema.rooms)
+      .leftJoin(schema.questions, eq(schema.questions.roomId, schema.rooms.id))
+      .groupBy(schema.rooms.id, schema.rooms.name)
       .orderBy(schema.rooms.createdAt)
 
     return results
