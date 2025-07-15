6faadf1977624f268a48f7b6b76e4c43b7508e88
jgbenine <joaobenine@gmail.com>
2025-07-15 11:07:10 -0300

    feat: adding new table to upload audio

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,18 @@
 // biome-ignore assist/source/organizeImports: <explanation>
+import { fastifyCors } from '@fastify/cors'
+import { fastifyMultipart } from '@fastify/multipart'
 import { fastify } from 'fastify'
 import {
   serializerCompiler,
   validatorCompiler,
   type ZodTypeProvider
 } from 'fastify-type-provider-zod'
-import { fastifyCors } from '@fastify/cors'
 import { env } from './env.ts'
-import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { createQuestionRoute } from './http/routes/create-question.ts'
 import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsQuestionsRoute } from './http/routes/get-room-questions.ts'
-import { createQuestionRoute } from './http/routes/create-question.ts'
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+import { uploadAudioRoute } from './http/routes/upload-audio.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -26,7 +28,10 @@ app.setValidatorCompiler(validatorCompiler)
 
 app.listen({ port: env.PORT })
 
+app.register(fastifyMultipart)
+
 app.register(getRoomsRoute)
 app.register(createRoomRoute)
 app.register(getRoomsQuestionsRoute)
 app.register(createQuestionRoute)
+app.register(uploadAudioRoute)
